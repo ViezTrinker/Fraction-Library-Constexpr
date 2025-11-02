98ffd582cbccb85fbc412601b044a64b2730e932
User <muhammed.omayrat@gmx.de>
2025-11-02 17:01:26 +0100

    Add project files.

---
+++ Fraction/fraction.h
@@ -0,0 +1,123 @@
+#ifndef FRACTION_H
+#define FRACTION_H
+
+#include <array>
+#include <numeric>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+
+template<typename T>
+class Fraction
+{
+public:
+	constexpr Fraction(const T numerator, const T denominator = 1) : _numerator(numerator), _denominator(denominator)
+	{	
+		if constexpr(std::is_signed<T>::value)
+		{
+			if (denominator < 0)
+			{
+				_numerator = -_numerator;
+				_denominator = -_denominator;
+			}
+		}
+
+		if (_numerator == 0)
+		{
+			_denominator = 1;
+		}
+
+		if (_denominator == 0)
+		{
+			throw std::invalid_argument("Denominator cannot be zero");
+		}
+
+		T greatestCommonDivisor = std::gcd(_numerator, _denominator);
+		_numerator = _numerator / greatestCommonDivisor;
+		_denominator = _denominator / greatestCommonDivisor;
+	}
+
+	constexpr Fraction operator+(const Fraction& other) const
+	{
+		T numerator = _numerator * other._denominator + _denominator * other._numerator;
+		T denominator = _denominator * other._denominator;
+		return Fraction{ numerator, denominator };
+	}
+
+	constexpr Fraction operator-(const Fraction& other) const
+	{
+		T numerator = _numerator * other._denominator - _denominator * other._numerator;
+		T denominator = _denominator * other._denominator;
+		return Fraction{ numerator, denominator };
+	}
+
+	constexpr Fraction operator*(const Fraction& other) const
+	{
+		T numerator = _numerator * other._numerator;
+		T denominator = _denominator * other._denominator;
+		return Fraction{ numerator, denominator };
+	}
+
+	constexpr Fraction operator/(const Fraction& other) const
+	{
+		T numerator = _numerator * other._denominator;
+		T denominator = _denominator * other._numerator;
+		return Fraction{ numerator, denominator };
+	}
+
+	constexpr bool operator==(const Fraction& other) const
+	{
+		return ((_numerator == other._numerator) && (_denominator == other._denominator));
+	}
+
+	constexpr bool operator!=(const Fraction& other) const
+	{
+		return !(*this == other);
+	}
+
+	constexpr bool operator>(const Fraction& other) const
+	{
+		return _numerator * other._denominator > other._numerator * _denominator;
+	}
+
+	constexpr bool operator>=(const Fraction& other) const
+	{
+		return _numerator * other._denominator >= other._numerator * _denominator;
+	}
+
+	constexpr bool operator<(const Fraction& other) const
+	{
+		return _numerator * other._denominator < other._numerator * _denominator;
+	}
+
+	constexpr bool operator<=(const Fraction& other) const
+	{
+		return _numerator * other._denominator <= other._numerator * _denominator;
+	}
+
+	constexpr double ToDouble(void) const
+	{
+		return static_cast<double>(_numerator) / static_cast<double>(_denominator);
+	}
+
+	constexpr double ToFloat(void) const
+	{
+		return static_cast<float>(_numerator) / static_cast<float>(_denominator);
+	}
+	
+	constexpr T GetNumerator(void) const
+	{
+		return _numerator;
+	}
+
+	constexpr T GetDenominator(void) const
+	{
+		return _denominator;
+	}
+
+private:
+	T _numerator;
+	T _denominator;
+};
+
+#endif // FRACTION_H
